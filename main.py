700ee76a309ca3b5cecf36f10c6632a572d6fb19
pop777off <63285537+pop777off@users.noreply.github.com>
2024-05-04 14:48:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ async def start():
         types.InlineKeyboardButton("Раздел 3", callback_data="section_3")
     )
 
+    section_1_keyboard = types.InlineKeyboardMarkup()
+    section_1_keyboard.add(types.InlineKeyboardButton("Пункт 1", callback_data="section_1_item_1"))
+    section_1_keyboard.add(types.InlineKeyboardButton("Пункт 2", callback_data="section_1_item_2"))
+    section_1_keyboard.add(types.InlineKeyboardButton("Пункт 3", callback_data="section_1_item_3"))
+    section_1_keyboard.add(types.InlineKeyboardButton("Пункт 4", callback_data="section_1_item_4"))
+
     @dp.message_handler(commands = ["start"])
     async def send_welcome(message: types.Message):
         await message.reply("Привет! Я бот!")
@@ -24,13 +30,23 @@ async def start():
     async def send_menu(message: types.Message):
         await message.reply("Выберите раздел:", reply_markup=menu_keyboard)
 
-    @dp.callback_query_handler(lambda c: c.data.startwith("section_"))
+    @dp.callback_query_handler(lambda c: c.data == "section_1")
+    async def show_section_1_menu(callback_query: types.CallbackQuery):
+        await bot.answer_callback_query(callback_query.id)
+        await bot.send_message(callback_query.from_user.id, "Выберите пункт из раздела 1:", reply_markup=section_1_keyboard)
+
+    @dp.callback_query_handler(lambda c: c.data.startswith("section_1_item_"))
+    async def process_section_1_selection(callback_query: types.CallbackQuery):
+        item = callback_query.data.split("_")[2]
+        await bot.answer_callback_query(callback_query.id)
+        await bot.send_message(callback_query.from_user.id, f"Вы выбрали пункт {item} из раздела 1")
+
+    @dp.callback_query_handler(lambda c: c.data in ["section_2", "section_3"])
     async def process_menu_selection(callback_query: types.CallbackQuery):
         section = callback_query.data.split("_")[1]
         await bot.answer_callback_query(callback_query.id)
         await bot.send_message(callback_query.from_user.id, f"Вы выбрали раздел {section}")
 
-        
 
     @dp.message_handler()
     async def on_message(message: types.Message):
